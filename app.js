9db61758c01f97a4a471dc0364fb697e066e8f93
Frank Liu <437730547@qq.com>
2018-03-31 16:19:41 +0000

    add mLab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes = require("./routes/comments");
 var campgroundRoutes = require("./routes/campgrounds");
 var indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://frankliuyh:yelpcamp@ds131329.mlab.com:31329/yelp_camp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
